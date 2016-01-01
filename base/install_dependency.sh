36cc5079a5c6f455b5bcd62b3eb4f45801f346dd
coraldane <coraldane@163.com>
2016-01-01 10:16:33 +0800

    Initial import

--- base/install_dependency.sh
+++ base/install_dependency.sh
@@ -5,14 +5,14 @@
 #cat /root/CentOS.repo > /etc/yum.repos.d/CentOS.repo
 
 yum -y install make automake autoconf file
+yum -y install libtool
+yum -y install gcc
 yum -y install gcc-c++
 yum -y install libuuid-devel
-yum -y install e2fsprogs-devel.x86_64
-yum -y install google-perftools
-yum -y install libtool
 yum -y install zlib-devel
+yum -y install mysql-devel
 yum -y install readline-devel
+yum -y install e2fsprogs-devel.x86_64
 yum -y install ncurses.x86_64
 yum -y install ncurses-devel.x86_64
-yum -y install mysql
-yum -y install mysql-devel
+yum -y install gperftools-devel.x86_64
