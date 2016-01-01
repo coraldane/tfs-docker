5e3c0b46df8a9912cf63475116e2f63c0a09d67a
coraldane <coraldane@163.com>
2016-01-01 10:27:04 +0800

    Initial import

--- centos7/scripts/build_tfs.sh
+++ centos7/scripts/build_tfs.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-echo export TBLIB_ROOT=/opt/tblib >> /root/.bash_profile
-source /root/.bash_profile
 
 cd /mnt
 tar -zxvf tb-common-utils.tar.gz && rm -rf tb-common-utils.tar.gz
-tar -zxvf tfs-release-2.2.0.tar.gz && rm -rf tfs-release-2.2.0.tar.gz
+#tar -zxvf tfs-release-2.2.0.tar.gz && rm -rf tfs-release-2.2.0.tar.gz
+tar -zxvf jemalloc-4.0.4.tar.gz && rm -rf jemalloc-4.0.4.tar.gz
+
+cd /mnt/jemalloc-4.0.4
+./configure && make && make install
+
+
+echo export TBLIB_ROOT=/opt/tblib >> /root/.bash_profile
+source /root/.bash_profile
+
 
 cd /mnt/tb-common-utils
 sh build.sh
 
-cd /mnt/tfs-release-2.2.0
+cd /mnt/tfs-release-2.2.16
 sh build.sh init
 ./configure --prefix=/opt/tfs --with-release --without-tcmalloc
 
---
+++ centos7/tars/.svn/pristine/47/472ee8fb8c5d1b04113402c8e4d80825b5f3582e.svn-base
@@ -0,0 +1,1 @@
+link ../packages/tfs.spec
